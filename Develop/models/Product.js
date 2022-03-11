fd63437e1babcb1baadf08ece5894b32b8975465
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-12 00:34:59 +0800

    Models done, routes started, env done

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      isDecimal: true, 
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+      isNumeric: true,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
@@ -21,3 +48,5 @@ Product.init(
 );
 
 module.exports = Product;
+
+
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "productId" });
+
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tagId" });
+
 
 // Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+// Product.belongsToMany(Tag, { through: ProductTag });
+
+// // Tags belongToMany Products (through ProductTag)
+
+// Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
@@ -18,3 +37,6 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+
