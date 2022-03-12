44097924a390a6490ef390e1d22db3d6166253e8
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-13 00:27:41 +0800

    routes done, im tired im going to bed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,32 +4,18 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
-
-// Categories have many Products
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "productId" });
 
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tagId" });
-
-
-// Products belongToMany Tags (through ProductTag)
-
-// Product.belongsToMany(Tag, { through: ProductTag });
-
-// // Tags belongToMany Products (through ProductTag)
-
-// Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
